247095bb8a315f6b83df63c1069d582431e27749
Deepam-Rai <deepmrai1@gmail.com>
2024-07-26 00:11:17 +0530

    Added better loggings.

--- RNN.py
+++ RNN.py
@@ -6,6 +6,8 @@ import torch
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
 import numpy as np
+from tqdm import tqdm
+
 from utils import get_text
 import torch.nn as nn
 from datetime import datetime
@@ -20,7 +22,7 @@ class TextDatasetPlus(Dataset):
     """
     def __init__(self, data_paths: List[Path], seq_length: int = 10, batch_size: int = 64):
         self.paths: List[Path] = data_paths
-        self.text: str = get_text(self.paths)
+        self.text: str = get_text(self.paths)[:1000]
         # self.text: str = self.process_text(self.text)
         self.tokens = list(set(self.tokenize_text(self.text)))
         self.vocab_size = len(self.tokens)
@@ -102,8 +104,9 @@ def train(dataloader, model, max_epochs, sequence_length, checkpoints: bool = Tr
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
     for epoch in range(1, max_epochs+1):
+        print(f"\nEPOCH : {epoch}")
         state = model.init_state(batch_size=batch_size)
-        for batch, (x, y) in enumerate(dataloader):
+        for batch, (x, y) in (pbar := tqdm(enumerate(dataloader))):
             optimizer.zero_grad()
 
             y_pred, state = model(x, state)
@@ -113,14 +116,18 @@ def train(dataloader, model, max_epochs, sequence_length, checkpoints: bool = Tr
 
             loss.backward()
             optimizer.step()
-            if batch % 1000 == 0:
-                print(f'epoch: {epoch}/{max_epochs} \t batch: {batch} \t loss: {loss.item()}')
+            pbar.set_description(f'epoch: {epoch}/{max_epochs} \t batch: {batch} \t loss: {loss.item()}')
         if checkpoints:
             torch.save(
                 model.state_dict(),
                 Path(f'./models/checkpoints/{datetime.now().strftime("%d_%m_%Y__%H_%M_%S")}_epochs_{epoch}_device_{device}')
             )
-            print(f"\nGenerated text\n{predict(dataset, model, eval_mode=False)}")
+            print(
+                f"Generated text:\n"
+                f"-------------------\n"
+                f"{predict(dataset, model, eval_mode=False)}\n"
+                f"-------------------\n"
+            )
 
 
 def predict(dataset, model, seed: str = None, gen_length=100, eval_mode: bool=True):
@@ -157,9 +164,9 @@ epochs = 10
 data_path = Path('./data/Shakespeare')
 dataset = TextDatasetPlus([data_path], seq_length=seq_length, batch_size=batch_size)
 vocab_size = dataset.vocab_size
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
 print(
-    f"Dataset:  {data_path}\n"
+    f"DATASET:  {data_path}\n"
     f"Vocab size: {dataset.vocab_size}  Corpus size: {len(dataset.text_as_token_ids)}"
     f"Number of batches:{len(dataloader)} \t batch_size = {batch_size} \t seq_length = {seq_length}"
 )
@@ -167,19 +174,19 @@ print(
 
 model = RNNGen(vocab_size=vocab_size, embed_dim=embed_dim, hidden_dim=hidden_dim, num_layers=num_layers).to(device)
 # model.load_state_dict(torch.load(Path('./models/checkpoints/25_07_2024__17_03_07_epochs_19_device_cuda'), weights_only=True))
-print(f"\nModel: {model}")
+print(f"\nMODEL: {model}")
 
 
 X, Y = next(iter(dataloader))
 y_pred, _ = model(X, model.init_state(batch_size=batch_size))
-print("\nPretrain performance:\n---------- Y - Original target")
+print("\nPRETRAIN PERFORMANCE:\n---------- Y - Original target")
 print(''.join([dataset.id_to_token[x] for x in Y[0].cpu().tolist()]))
 print("---------- Generated")
 print(''.join([random.choices(dataset.tokens, p.cpu().detach().numpy())[0] for p in y_pred[0]]))
 print("---------- END")
 
-print("\nStarting Training:")
+print("\nSTARTING TRAINING:")
 train(dataloader, model, max_epochs=epochs, sequence_length=seq_length)
-print("Training complete.\n")
+print("TRAINING COMPLETE.\n")
 
-print(f"\nSample generated text:\n{predict(dataset, model, gen_length=100)}")
+print(f"\nSAMPLE GENERATED TEXT:\n{predict(dataset, model, gen_length=100)}")
